8d88c23337564767826390084a56f87e56b391cf
qp <code@quietparty.net>
2025-01-13 05:38:19 +0000

    add website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import solidJs from '@astrojs/solid-js';
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://quietparty.net',
+  integrations: [mdx(), sitemap(), solidJs()],
+  adapter: netlify(),
 });
