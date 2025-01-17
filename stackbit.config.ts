f3048c6e3e120295ee99c566e62dd7cbb5e27825
chee <yay@chee.party>
2025-01-17 17:55:01 +0000

    add stackbit config

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,18 @@
-import { defineStackbitConfig } from '@stackbit/types';
-
-export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "astro",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+export default {
+	stackbitVersion: "~0.6.0",
+	ssgName: "custom",
+	nodeVersion: "22",
+	devCommand: "node_modules/.bin/astro dev --port {PORT} --hostname 127.0.0.1",
+	experimental: {
+		ssg: {
+			name: "Astro",
+			logPatterns: {
+				up: ["is ready", "astro"],
+			},
+			directRoutes: {
+				"socket.io": "socket.io",
+			},
+			passthrough: ["/vite-hmr/**"],
+		},
+	},
+}
